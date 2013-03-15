3dd288530dee817bb641536be3312edb77940d67
Shane McQuillan <shanemcquillan@live.com>
2013-03-14 21:01:36 -0400

    Heroku database config update

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var express = require('express'),		
 	mongoose = require('mongoose');
-	//everyauth = require('everyauth'); everyauth.debug = true;
-passport = require('passport');
 
+passport = require('passport');
 config = require('./config/config.json');		//NOTE: Global variable.
 
 var app = module.exports = express.createServer();
@@ -10,7 +9,9 @@ var app = module.exports = express.createServer();
 var MemoryStore = express.session.MemoryStore;
 sessionStore = new MemoryStore();
 
-mongoose.connect('mongodb://' + config.db_ip + '/' + config.db_name);
+var dbIP = process.env.MONGOLAB_URI || config.db_ip;
+var dbName = process.env.MONGOLAB_NAME || config.db_name;
+mongoose.connect('mongodb://' + dbIP + '/' + dbName);
 
 require('./config/auth.js')(passport);
 require('./config/environment.js')(app, express, passport, sessionStore);
